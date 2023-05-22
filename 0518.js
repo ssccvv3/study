fe75f307294632edd7fdfc86fc38a8103f2e8fbd
ssccvv3 <119987000+ssccvv3@users.noreply.github.com>
2023-05-22 17:53:10 +0900

    05-22 17:53
    work finish

--- 0518.js
+++ 0518.js
@@ -1,3 +1,3 @@
 console.log(1+1);
-parseInt() = 문자열을 숫자로 변경
-prompt("") = 창을 띄워 입력값을 그대로 반영
+parseInt() = "문자열을 숫자로 변경"
+prompt("") = "창을 띄워 입력값을 그대로 반영"
--- conditions/if.js
+++ conditions/if.js
@@ -8,6 +8,7 @@ if (random >= 0.5) {
     console.log(random);
 }
 
+const dayOfWork = prompt()
 const dayOfWeek = "Friday";
 
 if(dayOfWeek === "Monday") {
@@ -18,14 +19,16 @@ if(dayOfWeek === "Monday") {
     console.log("ITS FRIDAY!! ")
 }
 
-// 0-5 free
-// 5-10 child $10
-// 10-65 adult $20
-// 65+ senior $10
+0-5 free
+5-10 child $10
+10-65 adult $20
+65+ senior $10
 
-const age = 8;
+const age = 30;
 if (age < 5) {
     console.log(" You are a baby. you get in free!")
-} even if(age < 10) {
+} else if(age < 10) {
     console.log("you are a child you pay $10")
-}
+} else if(age < 65) {
+    console.log("you are a adult pay $20")
+} 
